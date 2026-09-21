96707bc25f3ad9c4b3f3a81db5f5f5a149e73888
JONES DARKO <jonesdarko345@gmail.com>
2026-09-21 13:03:10 +0000

    chore: rebrand project and update configuration
    - Rename project to "uenr-course-attendance"
    - Update Vite server configuration
    - Simplify environment variable template
    - Remove temporary build script and lockfile

--- tmp/check_imgs.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const content = fs.readFileSync("/tmp/manus_bundle.js", "utf8");
-const imgMatches = content.match(/\/manus-storage\/[^"'`)\s]+/g) || [];
-console.log("Images found in bundle:", Array.from(new Set(imgMatches)));
